8252215b903ece459ccaccc34f5125b3d3cf5dbe
Andrew <mzheng@andrew.cmu.edu>
2010-12-25 19:28:22 -0500

    Added GNU license

--- pandora/PandoraSource.py
+++ pandora/PandoraSource.py
@@ -1,3 +1,26 @@
+"""
+  rhythmbox-shoutcast plugin for rhythmbox application.
+  Copyright (C) 2009  Alexey Kuznetsov <ak@axet.ru>
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+  The author would like to extend thanks to Kevin Mehall, author of Pithos,
+  an standalone Pandora player written in python, the code of which was
+  extraordinarily useful in the creation of this code
+"""
 import rhythmdb, rb
 import gobject, gtk
 import gst
